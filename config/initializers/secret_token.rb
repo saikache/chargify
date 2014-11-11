9504bc948f29b8dd00bd759009e8aab9295335fe
saikache <saisagarkache@gmail.com>
2014-11-11 19:33:37 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chargify::Application.config.secret_token = 'ed826c5da322f03ed1386dba014c892af73d9ec91a3b6f36e676172ec4cc6a8ca35ef47cda05412b6b66fa6539f0a667f80ec463dc6bc9b0f2a613bf44aa3dd5'
